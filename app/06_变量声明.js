de67a4d21873af8ebf4a9cf9d55cfd29f5c9f9e5
wangyanqiang <wangyanqiang@sensoro.com>
2017-12-13 14:56:02 +0800

    update

--- app/06_变量声明.js
+++ app/06_变量声明.js
@@ -2,4 +2,24 @@
  * Created by wyq on 17/12/13.
  * TypeScript变量声明
  */
+function f() {
+    var a = 10;
+    return function g() {
+        var b = a + 1;
+        return b;
+    };
+}
+var g = f();
+console.log(g()); // returns 11;
+function f1() {
+    var a = 1;
+    a = 2;
+    var b = g();
+    a = 3;
+    return b;
+    function g() {
+        return a;
+    }
+}
+console.log(f1()); // returns 2
 //# sourceMappingURL=06_变量声明.js.map
--- app/06_变量声明.ts
+++ app/06_变量声明.ts
@@ -3,6 +3,28 @@
  * TypeScript变量声明
  */
 
+function f() {
+    var a = 10;
+    return function g() {
+        var b = a + 1;
+        return b;
+    }
+}
 
+var g = f();
+console.log(g()); // returns 11;
+
+function f1() {
+    var a = 1;
+    a = 2;
+    var b = g();
+    a = 3;
+    return b;
+    function g() {
+        return a;
+    }
+}
+
+console.log(f1()); // returns 2
 
 
