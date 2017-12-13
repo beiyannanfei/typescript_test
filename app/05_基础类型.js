7efd1879017af0b742675782da55640221710c3c
wangyanqiang <wangyanqiang@sensoro.com>
2017-12-13 10:08:45 +0800

    update

---
+++ app/05_基础类型.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wyq on 17/12/12.
+ */
+var isDone = false;
+var decLiteral = 6; //十进制
+var hexLiteral = 0xf00d; //十六进制
+var binaryLiteral = 10; //二进制
+var octalLiteral = 484; //八进制
+var myName = "bob";
+myName = "smith";
+var age = 30;
+var sentence = "hello, my name is " + myName + ",\n\n" + age + " years old."; //可以直接跨行(反引号`)
+//# sourceMappingURL=05_基础类型.js.map
---
+++ app/05_基础类型.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by wyq on 17/12/12.
+ */
+let isDone: boolean = false;
+
+let decLiteral: number = 6;     //十进制
+let hexLiteral: number = 0xf00d;    //十六进制
+let binaryLiteral: number = 0b1010; //二进制
+let octalLiteral: number = 0o744;   //八进制
+
+let myName: string = "bob";
+myName = "smith";
+let age: number = 30;
+let sentence: string = `hello, my name is ${myName},
+
+${age} years old.`; //可以直接跨行(反引号`)
+
+
+
+
+
