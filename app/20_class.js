895e0c76d280e1c03376eb2d3716244bfefecd70
wangyanqiang <wangyanqiang@sensoro.com>
2017-12-15 17:19:49 +0800

    update

--- app/20_class.js
+++ app/20_class.js
@@ -1,3 +1,14 @@
 /**
  * Created by wyq on 17/12/14.
  */
+class Greeter {
+    constructor(msg) {
+        this.greeting = msg;
+    }
+    greet() {
+        return `Hello, ${this.greeting}`;
+    }
+}
+let g20 = new Greeter("first ts class");
+console.log(g20.greeting); //=> first ts class
+console.log(g20.greet()); //=> Hello, first ts class
--- app/20_class.ts
+++ app/20_class.ts
@@ -1,4 +1,18 @@
 /**
  * Created by wyq on 17/12/14.
  */
+class Greeter {
+    greeting: string;
 
+    constructor(msg: string) {
+        this.greeting = msg;
+    }
+
+    greet() {
+        return `Hello, ${this.greeting}`;
+    }
+}
+
+let g20 = new Greeter("first ts class");
+console.log(g20.greeting);      //=> first ts class
+console.log(g20.greet());       //=> Hello, first ts class
